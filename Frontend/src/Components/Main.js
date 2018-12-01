a9e9381be6e62d0bb7ee5ad8446e39c89af9082f
venkat pushpak <venkat.pushpak5@gmail.com>
2018-12-01 10:50:57 -0800

    resolving conflicts 1

--- Frontend/src/Components/Main.js
+++ Frontend/src/Components/Main.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import AddEditMovie from './AddEditMovie.js'
 import Home from './Home.js'
 import ShowUsers from './ShowAllUserDetails.js';
-import Home from './Home.js';
+
 import Login from './Login.js';
 
 //Create a Main Component
