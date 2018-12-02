4df24ceb96388e1425ea7a1b51263d442acd6510
Sasank <sasank.matavalam@sjsu.edu>
2018-12-01 21:24:11 -0800

    signup page

--- Frontend/src/Components/Main.js
+++ Frontend/src/Components/Main.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import AddEditMovie from './AddEditMovie.js'
 import Home from './Home.js';
 import Login from './Login.js';
+import Signup from './Signup.js';
+import MainHome from './MainHome.js';
 
 //Create a Main Component
 class Main extends Component {
@@ -10,7 +12,9 @@ class Main extends Component {
         return(
                 <div>
                     {/*Render Different Component based on Route*/}
+                    <Route path = "/" component={MainHome} />
                     <Route path="/login" component={Login}/>
+                    <Route path="/signup" component={Signup} />
                     <Route path="/home" component={Home}/>
                     <Route path="/addEditMovie" component={AddEditMovie}/>
                 </div>
--- Frontend/src/Components/MainHome.js
+++ Frontend/src/Components/MainHome.js
@@ -3,9 +3,13 @@ import Navbar from './Navbar'
 import axios from 'axios';
 import url from '../serverurl';
 
-class Home extends Component {
+class MainHome extends Component {
     constructor(props){
         super(props);
+    }
+
+    componentWillMount(){
+        //check sessions
     }
 
 
@@ -18,4 +22,4 @@ class Home extends Component {
     }
 }
 
-export default Home;
+export default MainHome;
