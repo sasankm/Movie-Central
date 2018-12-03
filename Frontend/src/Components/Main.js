4fa3bf37eec4e05e054f296b275d77223b9868b0
venkat pushpak <venkat.pushpak5@gmail.com>
2018-12-02 18:15:39 -0800

    removing redundant Home and Login imports

--- Frontend/src/Components/Main.js
+++ Frontend/src/Components/Main.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import AddEditMovie from './AddEditMovie.js'
-import Home from './Home.js';
-import Login from './Login.js';
+//import Home from './Home.js';
+//import Login from './Login.js';
 import Signup from './Signup.js';
 import MainHome from './MainHome.js';
 import Home from './Home.js'
