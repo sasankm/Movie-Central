d8c5f5dce644b62e538e2a4919f45cc9db3cd084
Sasank <sasank.matavalam@sjsu.edu>
2018-12-05 13:35:38 -0800

    addded movie profile

--- Frontend/src/Components/Main.js
+++ Frontend/src/Components/Main.js
@@ -7,9 +7,7 @@ import Signup from './Signup.js';
 import MainHome from './MainHome.js';
 import ShowUsers from './ShowAllUserDetails.js';
 import UserProfile from './UserProfile.js';
-
 import Rating from './Rating/rating.js';
-
 import Stats from './Stats.js';
 import Scoreboard from './Scoreboard.js';
 import Payment from './Payment.js';
@@ -26,7 +24,7 @@ class Main extends Component {
                 <div>
                     {/*Render Different Component based on Route*/}
 
-                    <Route path = "/" exact component={MainHome} />
+                    <Route exact path = "/" exact component={MainHome} />
                     <Route path="/login" exact component={Login}/>
                     <Route path="/signup" exact component={Signup} />
                     <Route path="/home" exact component={Home}/>
@@ -35,15 +33,6 @@ class Main extends Component {
                     <Route path="/showusers/{userid}" exact component={ShowUsers}/>
                     <Route path="/profile" exact component={UserProfile}/>
                     <Route path="/rating" exact component={Rating}/>
-
-                    <Route exact path = "/" component={MainHome} />
-                    <Route path="/login" component={Login}/>
-                    <Route path="/signup" component={Signup} />
-                    <Route path="/home" component={Home}/>
-                    <Route path="/addEditMovie" component={AddEditMovie}/>
-                    <Route path="/showusers" component={ShowUsers}/>
-                    <Route path="/showusers/{userid}" component={ShowUsers}/>
-                    <Route path="/profile" component={UserProfile}/>
                     <Route path="/stats" component={Stats} />
                     <Route path="/scoreboard" component={Scoreboard}/>
                     <Route path="/payment" component={Payment}/>
--- Frontend/src/Components/MovieProfile.js
+++ Frontend/src/Components/MovieProfile.js
@@ -3,29 +3,28 @@ import YouTube from 'react-youtube';
 import swal from 'sweetalert';
 import Navbar from './Navbar';
 import url from '../serverurl';
+import '../css/movieprofile.css';
+import axios from 'axios';
 
 
 class MovieProfile extends React.Component {
 
     constructor(){
-
-
         super();
         this.state={
             name:"thissssss",
             id:"0KGP9f3duEg",
             youtubeId:"",
             result:"",
-            data:""
-
+            data:"",
+            type:""
         };
 
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleRating = this.handleRating.bind(this);
+        this.handlePay = this.handlePay.bind(this);
         this.handleClick=this.handleClick.bind(this);
-
     }
-
-
-
 
 
     componentDidMount() {
@@ -35,10 +34,8 @@ class MovieProfile extends React.Component {
             .then(
                 (result) => {
                     console.log("users from db",result);
-
                     var n=result.movie.split("=");
                     var Id=n[1];
-
                     this.setState({
                         isLoaded: true,
                         data: result,
@@ -56,33 +53,83 @@ class MovieProfile extends React.Component {
                     });
                 }
             )
+
+            axios.get(url + "/checksession")
+            .then((response) => {
+                if(response.data.status === "SUCCESS"){
+                    console.log("inside session", response.data);
+                this.setState({
+                    type: response.data.type
+                })
+                }
+            }).catch((error) => {
+                console.log(error);
+            })
     }
 
 
-
-
+    handleAdd(e){
+        console.log("in handle Add");
+        if(this.state.type === "ADMIN"){
+            this.props.history.push('/addEditmovie?movieid',{
+                params :{
+                 movieID : this.state.data.movieid
+                }
+             })
+        } else {
+            swal("You are not allowed to edit the movie", "", "warning")
+        }
+    }
 
     handleClick=(event)=>{
-
         console.log("clicked the video")
-        if(this.state.name=="thissssss"){
-             event.target.pauseVideo();
-            swal("Cant play video", "please pay and play", "warning");
-
-        }
+         fetch(url+"/play?movieid=1")
+            .then(
+                (result) => {
+                    console.log("play from db",result.body);
+                   // if(result.status==200)
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    console.log("error from play",error);
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
 
     }
+    handleRating(e){
+        console.log("in handle rating");
+        this.props.history.push('/rating', {
+            params :{
+                movieID : this.state.data.movieid
+               }
+        })
+    }
+
+    handlePay(e){
+        console.log("inside handle pay");
+        this.props.history.push('/payment', {
+            params : {
+                payType: 'PayPerViewOnly',
+                movieID: this.state.data.movieid,
+            }
+        })
+    }
+
     _onReady(event) {
         // access to player in all event handlers via event.target
-
-
              event.target.pauseVideo();
-
     }
 
 
 
     render() {
+        const url='https://youtube.com/embed/'+this.state.youtubeId;
         const opts = {
             height: '390',
             width: '640',
@@ -91,34 +138,87 @@ class MovieProfile extends React.Component {
             }
         };
         console.log("result ")
-if (this.state.data==""){
-    return null
-}
-else{
+        if (this.state.data==""){
+         return null
+    }
+    
+ else{
     return (
         <div>
-            <div>
-            <h1>Movie ID:</h1> {this.state.data.movieid}
-            <h1>Movie Name:</h1> {this.state.data.title}
-            <h1>Movie Actors:</h1> {this.state.data.actors}
-            <h1>Movie Director:</h1> {this.state.data.director}
-            <h1>Movie Availability:</h1> {this.state.data.availability}
-            <h1>Movie Year:</h1> {this.state.data.year}
-            <h1>Movie rating:</h1> {this.state.data.rating}
-            <h1>Movie Studio:</h1> {this.state.data.studio}
-
-            <h1>Movie Synopsis:</h1> {this.state.data.synopsis}
-
-            <h1>Movie Country:</h1> {this.state.data.country}
-            <h1>Movie Price:</h1> {this.state.data.price}
+            <Navbar />
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5">
+                    <table className="table table-hover" id="table2">
+                        <thead>
+                            <tr className='table-secondary'>
+                                <th>About Movie</th>
+                                <th>Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Movie ID:</td>
+                                <td>{this.state.data.movieid}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie Name:</td>
+                                <td>{this.state.data.title}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie Actors:</td>
+                                <td>{this.state.data.actors}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie Director:</td>
+                                <td>{this.state.data.director}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie Year:</td>
+                                <td>{this.state.data.year}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie rating:</td>
+                                <td>{this.state.data.rating}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie Studio:</td>
+                                <td>{this.state.data.studio}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie Synopsis:</td>
+                                <td>{this.state.data.synopsis}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie Country:</td>
+                                <td>{this.state.data.country}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie Price:</td>
+                                <td>{this.state.data.price}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                    <div class="col-lg-6">
+                    <button style={{backgroundColor : "red"}} onClick = {this.handleClick}  class="btn btn-primary"><b>Pay</b></button> 
+                    <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item"  src={url}></iframe>
+                    </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                    <button style={{backgroundColor : "red"}} onClick = {this.handleAdd}  class="btn btn-primary"><b>Add/Edit Movie</b></button> 
+                    </div>
+                    <div class="col-lg-4">
+                    <button style={{backgroundColor : "red"}} onClick = {this.handleRating}  class="btn btn-primary"><b>Rate Movie</b></button> 
+                    </div>
+                    <div class="col-lg-4">
+                    <button style={{backgroundColor : "red"}} onClick = {this.handlePay}  class="btn btn-primary"><b>Pay</b></button> 
+                    </div>
+                </div>
             </div>
-
-            <YouTube
-                videoId={this.state.youtubeId}
-                opts={opts}
-                onReady={this._onReady}
-                onPlay={this.handleClick}
-            />
         </div>
     );
 
